60c7e0ef5b9ffb22f31971d4cd182ed871cebf9c
Giolvani <inavloig@gmail.com>
2022-11-14 18:06:05 -0300

    create fixtures

---
+++ src/__fixtures__/card.ts
@@ -0,0 +1,16 @@
+import { Card } from "models/Card";
+
+export const CardOne: Card = {
+  id: 1,
+  description: "Mow the lawn",
+};
+
+export const CardTwo: Card = {
+  id: 2,
+  description: "Pull weeds",
+};
+
+export const CardThree: Card = {
+  id: 3,
+  description: "Rake the leaves",
+};
---
+++ src/__fixtures__/phase.ts
@@ -0,0 +1,22 @@
+import { Phase } from "models/Phase";
+import { CardOne, CardTwo, CardThree } from "./card";
+
+export const PhaseOne: Phase = {
+  id: 1,
+  title: "To Do",
+  cards: [CardOne],
+};
+
+export const PhaseTwo: Phase = {
+  id: 2,
+  title: "In progress",
+  cards: [CardTwo],
+};
+
+export const PhaseThree: Phase = {
+  id: 3,
+  title: "Done",
+  cards: [CardThree],
+};
+
+export const Phases: Phase[] = [PhaseOne, PhaseTwo, PhaseThree];
