8b94d205c23dc3065373c2a01822c70c4eeb5019
Giolvani <inavloig@gmail.com>
2022-11-14 18:07:14 -0300

    list phases and cards

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,14 +1,43 @@
+import { useMemo, useState } from "react";
 import { Form } from "components/Form";
-import { Phase } from "components/Phase";
+import { Phase as PhaseComponent } from "components/Phase";
+import { Phase as PhaseType } from "models/Phase";
 import { Container } from "./styled";
+import { Phases as PhaseList } from "../../__fixtures__/phase";
 
 export function Board() {
+  const [phases] = useState<PhaseType[]>(PhaseList);
+
+  const isFirstPhase = useMemo(
+    () =>
+      (phase: PhaseType): boolean => {
+        return JSON.stringify(phases[0]) === JSON.stringify(phase);
+      },
+    [phases]
+  );
+
+  const isLastPhase = useMemo(
+    () =>
+      (phase: PhaseType): boolean => {
+        return (
+          JSON.stringify(phases[phases.length - 1]) === JSON.stringify(phase)
+        );
+      },
+    [phases]
+  );
+
   return (
     <Container>
       <div className="phases">
-        <Phase title="To Do" />
-        <Phase title="In Progress" />
-        <Phase title="Done" />
+        {phases.map((phase) => (
+          <PhaseComponent
+            key={phase.id}
+            title={phase.title}
+            cards={phase.cards}
+            isFirst={isFirstPhase(phase)}
+            isLast={isLastPhase(phase)}
+          />
+        ))}
       </div>
       <Form />
     </Container>
--- src/components/Phase/Phase.tsx
+++ src/components/Phase/Phase.tsx
@@ -1,17 +1,28 @@
-import { Card } from "components/Card";
+import { Card as CardComponent } from "components/Card";
+import { Card as CardType } from "models/Card";
 import { Container, Title } from "./styled";
 
 type PhaseType = {
   title: string;
+  isFirst?: boolean;
+  isLast?: boolean;
+  cards?: CardType[];
 };
 
 export function Phase(props: PhaseType) {
-  const { title } = props;
+  const { cards, title, isFirst, isLast } = props;
 
   return (
     <Container>
       <Title>{title}</Title>
-      <Card />
+      {cards?.map((card) => (
+        <CardComponent
+          key={card.id}
+          description={card.description}
+          disableRewind={isFirst}
+          disableForward={isLast}
+        />
+      ))}
     </Container>
   );
 }
