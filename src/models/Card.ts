d19493950dad02b15f67d9cd01f24b49c04fa5b7
Giolvani <inavloig@gmail.com>
2022-11-14 18:05:52 -0300

    create models

---
+++ src/models/Card.ts
@@ -0,0 +1,4 @@
+export type Card = {
+  id: number;
+  description: string;
+};
---
+++ src/models/Phase.ts
@@ -0,0 +1,7 @@
+import { Card } from "./Card";
+
+export type Phase = {
+  id: number;
+  title: string;
+  cards: Card[];
+};
