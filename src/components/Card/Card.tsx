8727b5de747d24d3d5afbc1c288663b7e8ee8861
Giolvani <inavloig@gmail.com>
2022-11-14 17:27:27 -0300

    add controls to move card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,17 +1,29 @@
 import { Button } from "components/Button";
 import { Actions, Container, Description } from "./styled";
 
-export function Card() {
+type CardType = {
+  disableForward?: boolean;
+  disableRewind?: boolean;
+};
+
+export function Card(props: CardType) {
+  const { disableRewind, disableForward } = props;
+
   return (
     <Container>
       <Actions>
-        <Button color="red" icon title="Previous phase">
+        <Button
+          color="red"
+          icon
+          title="Previous phase"
+          disabled={disableRewind}
+        >
           &lsaquo;
         </Button>
       </Actions>
       <Description>Card description</Description>
       <Actions>
-        <Button color="green" icon title="Next phase">
+        <Button color="green" icon title="Next phase" disabled={disableForward}>
           &rsaquo;
         </Button>
       </Actions>
